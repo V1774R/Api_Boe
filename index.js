e7e1333935181a0ea2dc144b3dfb33187c0307f1
Victor Almeida <157659240+V1774R@users.noreply.github.com>
2024-07-14 17:33:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jsonServer = require("json-server"); // importing json-server library
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -6,5 +7,5 @@ const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
-
+server.use(cors());
 server.listen(port);
