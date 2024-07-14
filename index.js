04ab85e508612ecc87e0e65f559ca763fa73bf57
Victor Almeida <157659240+V1774R@users.noreply.github.com>
2024-07-14 17:39:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080;
 
+// Adiciona middlewares padrão do JSON Server
 server.use(middlewares);
-server.use(router);
+
+// Habilita CORS para todas as origens
 server.use(cors());
-server.listen(port);
+
+// Adiciona o roteador JSON Server
+server.use(router);
+
+// Inicia o servidor
+server.listen(port, () => {
+  console.log(`JSON Server está rodando na porta ${port}`);
+});
